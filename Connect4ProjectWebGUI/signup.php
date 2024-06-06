7d16fcb3ea4d39a5a041e876a2d3ef2632d27070
Aurelia Sindhu <100260518+AureliaSindhu@users.noreply.github.com>
2024-06-06 15:41:07 -0700

    merged with updated login

--- Connect4ProjectWebGUI/signup.php
+++ Connect4ProjectWebGUI/signup.php
@@ -3,19 +3,37 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Signup</title>
+    <link rel="stylesheet" href="signup.css">
 </head>
 <body>
 
 <div class="signup-container">
     <?php include "signup-loginmethod.php" ?>
      <form method = "POST">
+        <div class="heading">
+            <h1>welcome to connect!</h1>
+        </div>
         <div class="form-group">
-            <label for="">Username</label>
-            <input type="text" id="Username" name="Username" required>
-            <label for="">Password</label>
-            <input type="text" id="Password" name="Password" required> 
-            <button type="submit" class="btn" name="signup">Signup</button>
+            <div class="username">
+                <label for="">First Name</label>
+                <input type="text" id="Firstname" name="Firstname" required>
+            </div>
+            <div class="pass">
+                <label for="">Last Name</label>
+                <input type="text" id="Lastname" name="Lastname" required> 
+            </div>
+            <div class="pass">
+                <label for="">username</label>
+                <input type="text" id="Username" name="Username" required> 
+            </div>
+            <div class="pass">
+                <label for="">password</label>
+                <input type="text" id="Password" name="Password" required> 
+            </div>
+        </div>
+        <div class="group2">
+            <button type="submit" class="btn" name="signup">Sign Up</button>
         </div>
     </form>
 </div>
